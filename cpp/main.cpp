19943fe7a24d6ba375cbc031b5ab66e271335541
SamuelHiron <samuel.hironcarme@gmail.com>
2025-02-28 15:01:14 +0100

    split_bloc solved with jmp same & other bb done

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -6,7 +6,7 @@
 
 size_t current_id_block ; // On en a besoin pour les 2 structs
 std::unordered_map<size_t, u_int8_t> binary_contents; // On en a besoin aussi dans le main
-
+int bloc_null = 0;
 struct CSH {
     csh handle;
 
@@ -32,7 +32,6 @@ struct CSH {
 struct BasicBlock {
     size_t id;
     size_t start_address;
-    size_t end_address;
     bool end;
     std::vector<size_t> ids_successors;
     std::vector<cs_insn> instructions;
@@ -40,13 +39,29 @@ struct BasicBlock {
         : id(current_id_block), start_address(start_address), end(false) {
         current_id_block++;
     }
+    BasicBlock(size_t start_address,  std::vector<cs_insn> instructions, std::vector<size_t> ids_successors)
+        : id(current_id_block), start_address(start_address), instructions(instructions), ids_successors(ids_successors), end(false) {
+        current_id_block++;
+    }
 
     void print_BasicBlock() const {
-        std::cout << "Basic Block " << id << std::endl;
+        
+        std::cout << "Basic Block n°" << id ;
+        std::cout << " | Parents de :";
+        for(const auto & id_successor : ids_successors){
+            std::cout << " "<< id_successor;
+        }
+        std::cout<< std::endl;
         for (const auto& insn : instructions) {
             std::cout << "0x" << std::hex << insn.address << std::dec << ": "
                       << insn.mnemonic << " " << insn.op_str << std::endl;
         }
+        if (instructions.empty()){
+            std::cout << std::hex << "\n start_address: 0x" << start_address<< std::dec  << std::endl;
+            std::cout << "nombre de blocs dans le même état = " << bloc_null << std::endl;
+            bloc_null++;
+        }
+
         std::cout << std::endl;
     }
 };
@@ -88,8 +103,8 @@ struct RecursiveDescent {
             explore_BasicBlock(index_block);
             index_block++;
         }
-        std::cout << "Fin de l'exploration" << std::endl;
-        std::cout << "Nombre de blocs: " << blocks.size() << std::endl;
+        std::cout << "\n________________________________________________\n\nFin de l'exploration" << std::endl;
+        std::cout << "Nombre de blocs trouvés: " << blocks.size()<< "\n" << std::endl;
 
         for (const auto& block : blocks) {
             block.print_BasicBlock();  
@@ -97,57 +112,79 @@ struct RecursiveDescent {
         return 0;
     }
 
+    int split_BasicBlock(size_t id_basic_bloc_to_split, size_t split_address){
+        std::vector<cs_insn> debut_split_instructions;
+        std::vector<cs_insn> fin_split_instructions;
+ 
+        int i = 0;
+        while (i< blocks[id_basic_bloc_to_split].instructions.size()){
+            if(blocks[id_basic_bloc_to_split].instructions[i].address < split_address){
+                debut_split_instructions.push_back(blocks[id_basic_bloc_to_split].instructions[i]);
+            } else {
+                fin_split_instructions.push_back(blocks[id_basic_bloc_to_split].instructions[i]);
+            }
+            i++;
+        }
+        auto block_successor = BasicBlock{split_address, fin_split_instructions, blocks[id_basic_bloc_to_split].ids_successors};  // création d'un nouveau bloc
+        basic_block_start_address[split_address] = block_successor.id - 1; // pour garder la 1ere adresse d'un bloc associée à son id
+        blocks[id_basic_bloc_to_split].instructions = debut_split_instructions;
+        blocks[id_basic_bloc_to_split].ids_successors.clear();
+        blocks.push_back(block_successor);
+        return 0;     
+    }
+
     int explore_BasicBlock(const int index_block) {
         std::cout << "Exploring Basic Block " << blocks[index_block].id
                   << std::endl;
         auto current_address = blocks[index_block].start_address;
         if (addr2block.count(current_address)) {
-            std::cout
-                << "Je ne vois pas comment ça peut arriver mais soyons prudent"
-                << std::endl;
-            blocks[index_block].end = true;
+            std::cout << "Ce block" << addr2block[current_address] << " a déjà vu cette adresse " << current_address  << std::endl; 
+            blocks[index_block].end = true; // dans le cas split d'un bloc déjà vu
         }
+
         // int i =0; si boucle infinie
         while (!blocks[index_block].end) {  // par défaut initialisé à false
+ 
             // if (i == 5) {
             //   blocks[index_block]end = true;
             // }
             // i++;
+            addr2block[current_address] = index_block;
+
             std::cout << "Exploring address: 0x" << std::hex << current_address
                       << std::dec << std::endl;
             // On décode 1 instruction
-            std::array<u_int8_t, 16> bytes;
+           std::array<u_int8_t, 16> bytes;
             for (size_t i = 0; i < 16; i++) {
                 bytes[i] = binary_contents[current_address + i];
             }
             cs_insn* insn_tab;
-            std::cout << "ICI" << std::endl;
             size_t count = cs_disasm(handle, bytes.data(), bytes.size(),
                                      current_address, 1, &insn_tab);
-            std::cout << "LA" << std::endl;
             assert(count == 1);  // On s'occupe d'une instruction à la fois et
                                  // ça c'est bien passé
             auto insn = insn_tab[0];
             blocks[index_block].instructions.push_back(insn);  // on la stocke
+
             addr2block[current_address] =
                 blocks[index_block].id;  // on note qu'on a traité cette adresse
             auto next_address =
                 insn.address + insn.size;  // on prépare la prochaine adresse
             if (cs_insn_group(handle, &insn, CS_GRP_CALL) ||
                 cs_insn_group(handle, &insn, CS_GRP_JUMP)) {
-                blocks[index_block].end = true;
+                blocks[index_block].end = true;              
+                
                 // 2 potentiellement nouveaux blocs à créer
                 // le bloc juste après l'appel commence à next_address
                 if (insn.id != X86_INS_JMP && insn.id != X86_INS_LJMP) {
                     if (!basic_block_start_address.count(next_address) &&
-                        !addr2block.count(
-                            next_address)) {  // cas où l'adresse n'a jamais été
+                        !addr2block.count(next_address)) {  // cas où l'adresse n'a jamais été
                                               // traitée et elle n'est pas en
                                               // début de bloc
                         auto block_successor = BasicBlock{
                             next_address};  // création d'un nouveau bloc
                         basic_block_start_address[next_address] =
-                            block_successor.id - 1;
+                            block_successor.id - 1; // pour garder la 1ere adresse d'un bloc associée à son id
                         blocks[index_block].ids_successors.push_back(
                             block_successor.id);
                         blocks.push_back(block_successor);
@@ -157,19 +194,20 @@ struct RecursiveDescent {
                                                      // mais elle n'a pas encore
                                                      // été traitée
                         blocks[index_block].ids_successors.push_back(
-                            addr2block[next_address]);  // on ajoute l'id du
+                            basic_block_start_address[next_address]);  // on ajoute l'id du
                                                         // bloc existant à la
                                                         // liste des successeurs
-                                                        // de ce bloc
+                                                        // de ce bloc                
                     }
                 }
 
                 // le bloc loin
                 const auto& op = insn.detail->x86.operands[0];
                 if (op.type == X86_OP_IMM) {  // cas où l'instruction contient
-                                              // l'adresse de l'appel
+                                              // l'adresse de l'appel                 
                     std::cout << "0x" << std::hex << op.imm << std::dec
-                              << "X86_OP_IMM" << std::endl;
+                              << "  X86_OP_IMM" << std::endl;            
+
                     if (!basic_block_start_address.count(op.imm) &&
                         !addr2block.count(
                             op.imm)) {  // cas où l'adresse n'a jamais été
@@ -177,43 +215,56 @@ struct RecursiveDescent {
                                         // bloc
                         auto block_successor = BasicBlock{static_cast<size_t>(
                             op.imm)};  // création d'un nouveau bloc
-                        basic_block_start_address[next_address] =
-                            block_successor.id - 1;
+                        basic_block_start_address[static_cast<size_t>(op.imm)] =
+                            block_successor.id - 1; // pour garder la 1ere adresse d'un bloc associée à son id
                         blocks[index_block].ids_successors.push_back(
                             block_successor.id);
+                        
                         blocks.push_back(block_successor);
+
                     } else if (basic_block_start_address.count(
                                    op.imm)) {  // cas où l'adresse est déjà le
                                                // début d'un bloc mais elle n'a
                                                // pas encore été traitée
-                        blocks[index_block].ids_successors.push_back(
-                            addr2block[op.imm]);  // on ajoute l'id du bloc
-                                                  // existant à la liste des
+                       blocks[index_block].ids_successors.push_back(basic_block_start_address[op.imm]);  // on ajoute l'id du bloc
+                                                 // existant à la liste des
                                                   // successeurs de ce bloc
                     } else {  // cas où l'adresse est déjà traitée
                         auto id_basic_bloc_to_split =
                             addr2block[op.imm];  // Problème ici Bloc à split
-                    }
+                        size_t split_address = static_cast<size_t>(op.imm);
+                        std::cout << "bloc à split est n°" << id_basic_bloc_to_split << "à l'adresse 0x" << std::hex<< op.imm << std::dec <<  std::endl;
+                        split_BasicBlock(id_basic_bloc_to_split, split_address);
+                        std::cout << "index_block "<< index_block << "current_id_block - 1"<< current_id_block - 1 << std::endl; 
+                        blocks[index_block].ids_successors.push_back(current_id_block - 1);// on a un successeur
+                        if(index_block == id_basic_bloc_to_split){
+                         blocks[current_id_block-1].ids_successors.push_back(current_id_block - 1);   
+                        }
+                    }           
                 } else if (op.type == X86_OP_MEM) {
-                    std::cout << "0x" << std::hex << std::dec << "X86_OP_MEM"
+                    std::cout << "0x" << std::hex << std::dec << "  X86_OP_MEM"
                               << std::endl;
                 } else if (op.type == X86_OP_REG) {
-                    std::cout << "0x" << std::hex << std::dec << "X86_OP_REG"
+                    std::cout << "0x" << std::hex << std::dec << "  X86_OP_REG"
                               << std::endl;
                 }
-                std::cout << "CALL ou JUMP" << std::endl;
+
+                // std::cout << "CALL ou JUMP" << std::endl;
             } else if (cs_insn_group(handle, &insn, CS_GRP_RET) ||
                        cs_insn_group(handle, &insn, CS_GRP_INT)) {
-                std::cout << "RET ou INT" << std::endl;
+                // std::cout << "RET ou INT" << std::endl;
                 blocks[index_block].end = true;
             }
+            // if (!basic_block_start_address.count(next_address)){  
             current_address = next_address;
-
+            // } else {
+            //     blocks[index_block].end = true;
+            // }
             cs_free(
                 insn_tab,
                 count);  // manière + belle de le faire existe cf Jack's code
         }
-        blocks[index_block].print_BasicBlock();
+        //blocks[index_block].print_BasicBlock();
 
         return 0;
     }
